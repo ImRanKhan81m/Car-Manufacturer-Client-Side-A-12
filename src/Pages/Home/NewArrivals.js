fa43fc8288640ea9f5587e9f929a9a2555aba994
Imran Hossen <freelancerimran81@gmail.com>
2022-05-22 14:41:37 +0600

    updated:

--- src/Pages/Home/NewArrivals.js
+++ src/Pages/Home/NewArrivals.js
@@ -3,13 +3,15 @@ import img1 from '../../assets/Car tools/9-270x270.jpg'
 import img2 from '../../assets/Car tools/12-270x270.jpg'
 import img3 from '../../assets/Car tools/10-270x270.jpg'
 import img4 from '../../assets/Car tools/11-270x270.jpg'
+import img5 from '../../assets/Car tools/bn2.jpg'
+import img6 from '../../assets/Car tools/bn3.jpg'
 
 const NewArrivals = () => {
     return (
-        <div className='py-10'>
+        <div className='py-10 mid-content'>
             <h2 className='text-center text-primary  font-bold'>TOP NEW IN THE WEEK</h2>
             <h1 className='text-center text-3xl font-bold'>NEW ARRIVALS</h1>
-            <div className='grid grid-cols-4 gap-5 mid-content pt-10'>
+            <div className='grid grid-cols-4 gap-5  pt-10'>
                 <div class="card bg-base-100 shadow-xl border">
                     <figure class="p-0">
                         <img src={img1} alt="Shoes" class="rounded-xl" />
@@ -76,6 +78,15 @@ const NewArrivals = () => {
                         </div>
                         <p className='font-bold text-xl'>$96.00</p>
                     </div>
+                </div>
+            </div>
+
+            <div className='grid grid-cols-2 mt-20 w-full gap-10'>
+                <div>
+                    <img className='w-full' src={img5} alt="" />
+                </div>
+                <div>
+                    <img className='w-full'src={img6} alt="" />
                 </div>
             </div>
         </div>
