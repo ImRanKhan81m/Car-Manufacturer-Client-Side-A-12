101f6e619021b68e0f03d2952cb0fb38a5c407e4
Imran Hossen <freelancerimran81@gmail.com>
2022-05-22 19:46:15 +0600

    banner section updated

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div class="hero h-[400px]" style={{backgroundImage: `url("https://i.ibb.co/xL4VCbb/car-parts-repair-garage222.jpghttps://i.ibb.co/X34m12N/car-engine-1373-184.jpg")`}}>
-            <div class="hero-overlay bg-opacity-60"></div>
-            <div class="hero-content text-center text-neutral-content">
+        <div class="hero lg:h-[600px] md:h-[400px] sm:h-[400px] h-[400px]" style={{backgroundImage: `url("https://i.ibb.co/cbcNs1L/master-collects-rebuilt-motor-car-109285-4650.webp")`}}>
+            <div class="hero-overlay bg-opacity-40 bg-black"></div>
+            <div class="hero-content text-center text-white">
                 <div class="max-w-md">
-                    <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+                    <h1 class="mb-5 text-5xl font-bold">Hello There</h1>
                     <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button class="btn btn-primary">Get Started</button>
                 </div>
