419c80e7f8fe1d1bd1de6788f45c85c96b8982e8
Imran Hossen <freelancerimran81@gmail.com>
2022-05-22 15:29:40 +0600

    Categories shop section done

---
+++ src/Pages/Home/CategoriesShop.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import img1 from '../../assets/Car tools/cate1-180x210.jpg'
+import img2 from '../../assets/Car tools/1-270x270.jpg'
+import img3 from '../../assets/Car tools/5-150x150.jpg'
+import img4 from '../../assets/Car tools/5-270x270.jpg'
+import img5 from '../../assets/Car tools/6-270x270.jpg'
+import img6 from '../../assets/Car tools/cate5-180x210.jpg'
+
+const CategoriesShop = () => {
+    return (
+        <div className='bg-secondary my-20 py-16'>
+            <p className='text-primary font-bold text-center'>TOP FEATURED COLLECTIONS</p>
+            <h1 className='text-center font-bold text-3xl mb-10'>SHOP BY CATEGORIES</h1>
+            <div className='grid grid-cols-3 mid-content gap-5'>
+                <div class="card card-side bg-base-100 shadow-xl">
+                    <figure><img src={img1} alt="Movie" /></figure>
+                    <div class="card-body py-10 px-5">
+                        <h2 class="card-title font-bold">WHEELS & TIRES</h2>
+                        <li>Tires Chains</li>
+                        <li>Tires Accessories</li>
+                        <li>Wheels</li>
+                        <li>Wheels Accessories</li>
+                        <li>Tires</li>
+                        <div class="card-actions justify-start mt-3">
+                            <button class="btn btn-primary">View More</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-side bg-base-100 shadow-xl">
+                    <figure><img src={img2} alt="Movie" /></figure>
+                    <div class="card-body py-10 px-5">
+                        <h2 class="card-title font-bold">WHEELS & TIRES</h2>
+                        <li>Tires Chains</li>
+                        <li>Tires Accessories</li>
+                        <li>Wheels</li>
+                        <li>Wheels Accessories</li>
+                        <li>Tires</li>
+                        <div class="card-actions justify-start mt-3">
+                            <button class="btn btn-primary">View More</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-side bg-base-100 shadow-xl">
+                    <figure><img src={img3} alt="Movie" /></figure>
+                    <div class="card-body py-10 px-5">
+                        <h2 class="card-title font-bold">WHEELS & TIRES</h2>
+                        <li>Tires Chains</li>
+                        <li>Tires Accessories</li>
+                        <li>Wheels</li>
+                        <li>Wheels Accessories</li>
+                        <li>Tires</li>
+                        <div class="card-actions justify-start mt-3">
+                            <button class="btn btn-primary">View More</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-side bg-base-100 shadow-xl">
+                    <figure><img src={img4} alt="Movie" /></figure>
+                    <div class="card-body py-10 px-5">
+                        <h2 class="card-title font-bold">WHEELS & TIRES</h2>
+                        <li>Tires Chains</li>
+                        <li>Tires Accessories</li>
+                        <li>Wheels</li>
+                        <li>Wheels Accessories</li>
+                        <li>Tires</li>
+                        <div class="card-actions justify-start mt-3">
+                            <button class="btn btn-primary">View More</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-side bg-base-100 shadow-xl">
+                    <figure><img src={img5} alt="Movie" /></figure>
+                    <div class="card-body py-10 px-5">
+                        <h2 class="card-title font-bold">WHEELS & TIRES</h2>
+                        <li>Tires Chains</li>
+                        <li>Tires Accessories</li>
+                        <li>Wheels</li>
+                        <li>Wheels Accessories</li>
+                        <li>Tires</li>
+                        <div class="card-actions justify-start mt-3">
+                            <button class="btn btn-primary">View More</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-side bg-base-100 shadow-xl">
+                    <figure><img src={img6} alt="Movie" /></figure>
+                    <div class="card-body py-10 px-5">
+                        <h2 class="card-title font-bold">WHEELS & TIRES</h2>
+                        <li>Tires Chains</li>
+                        <li>Tires Accessories</li>
+                        <li>Wheels</li>
+                        <li>Wheels Accessories</li>
+                        <li>Tires</li>
+                        <div class="card-actions justify-start mt-3">
+                            <button class="btn btn-primary">View More</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CategoriesShop;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import CategoriesShop from './CategoriesShop';
 import NewArrivals from './NewArrivals';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner/>
             <NewArrivals/>
+            <CategoriesShop/>
         </div>
     );
 };
