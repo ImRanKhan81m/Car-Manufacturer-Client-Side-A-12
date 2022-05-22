dc2b7da1852b73a7371f69b2dfea98e1d0cebea1
Imran Hossen <freelancerimran81@gmail.com>
2022-05-22 19:31:35 +0600

    footer responsive done

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
 
 
     return (
-        <div className='pt-20 bg-secondary'>
-            <div className=' mid-content grid grid-cols-2 gap-5'>
+        <div className='lg:pt-20 md:pt-10 sm:pt-5 pt-5 bg-secondary'>
+            <div className=' mid-content grid lg:grid-cols-2 md:grid-cols-2 gap-5'>
                 <div>
                     <h1 className='font-bold text-2xl mb-5'>CONTACT US</h1>
                     <p> <b>Location :</b> 5611 Wellington Road, Suite 115, Gainesville, VA 20155.</p>
@@ -17,7 +17,7 @@ const Footer = () => {
                     <p><b>Working Time :</b> 7 Days a week from 10-00 am to 8.00 pm </p>
                 </div>
 
-                <div className='grid grid-cols-2'>
+                <div className='grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2' >
                     <div>
                         <h1 className='font-bold text-2xl mb-5'>My Account</h1>
                         <p>Brands</p>
@@ -28,7 +28,7 @@ const Footer = () => {
                         <p>Custom Link</p>
                     </div>
 
-                    <div>
+                    <div className='mt-5 lg:mt-0 md:mt-0 sm:mt-0'>
                         <h1 className='font-bold text-2xl mb-5'>INFORMATION</h1>
                         <p>Brands</p>
                         <p>Gift Certificates</p>
@@ -39,7 +39,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className='copyright text-center py-10 mt-5'>
+            <div className='copyright text-center lg:py-10 md:py-5 sm:py-5 pb-3 mt-5'>
                 <p>Copyright &copy; {getCurrentYear()}-{getCurrentYear() + 2}.  All Rights Reserved by <span className='text-primary'>CarTools Manufacturer</span> </p>
             </div>
         </div>
