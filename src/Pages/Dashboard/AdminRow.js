36aa5abacf2f0dbfe7e9dad97a14a17c4eb27314
Imran Hossen <freelancerimran81@gmail.com>
2022-05-25 01:52:49 +0600

    updated

--- src/Pages/Dashboard/AdminRow.js
+++ src/Pages/Dashboard/AdminRow.js
@@ -3,22 +3,29 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { toast } from 'react-toastify';
 
-const AdminRow = ({user, index, refetch}) => {
+const AdminRow = ({ user, index, refetch }) => {
 
-    const {email, role} = user;
+    const { email, role } = user;
 
-    const makeAdmin = () =>{
-        fetch(`http://localhost:5000/user/admin/${email}`,{
-            method:'PUT',
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
             headers: {
                 'authorization': `Bearer ${localStorage.getItem('accessToken')}`
             }
         })
-        .then(res=> res.json())
-        .then(data => {
-            refetch();
-            toast.success(  `Successfully is an admin`)
-        })
+            .then(res => {
+                if(res.status === 403){
+                    toast.error('Failed to Make an admin')
+                }
+                return res.json()
+            })
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    toast.success(`Successfully is an admin`)
+                }
+            })
     }
 
     return (
@@ -26,7 +33,7 @@ const AdminRow = ({user, index, refetch}) => {
             <th>{++index}</th>
             <td>{user.email}</td>
 
-            <td>{ role === 'admin'? <button onClick={makeAdmin} className='btn btn-sm btn-success'>Admin</button> : <button onClick={makeAdmin} className='btn btn-sm btn-secondary'>Make Admin</button>}</td>
+            <td>{role === 'admin' ? <button onClick={makeAdmin} className='btn btn-sm btn-success'>Admin</button> : <button onClick={makeAdmin} className='btn btn-sm btn-secondary'>Make Admin</button>}</td>
 
             <td><button className='btn btn-error btn-sm'><FontAwesomeIcon className='mr-2' icon={faTrash}></FontAwesomeIcon> Delete</button></td>
         </tr>
